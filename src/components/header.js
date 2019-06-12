6fd5c1ef4e47a7803657a060b538abe6ad73ab99
Joe Weaver <joeweaver@protonmail.com>
2019-06-11 21:27:03 -0700

    open social media links in new window

--- src/components/header.js
+++ src/components/header.js
@@ -12,11 +12,11 @@ const Header = ({ siteTitle }) => (
   >
     <nav class="social">
       <div>Connect with <span>Delek Children&rsquo;s Foundation</span></div>
-      <a href="https://vimeo.com/24754486"><i class="fab fa-vimeo"></i></a>
-      <a href="https://www.facebook.com/Delek-Childrens-Foundation-413781628796613/"><i class="fab fa-facebook-square"></i></a>
-      <a href="https://www.youtube.com/channel/UC7K3C7iSY1JO0HAy4P-Jn5w"><i class="fab fa-youtube"></i></a>
-      <a href="https://twitter.com/delekcf"><i class="fab fa-twitter"></i></a>
       <a class="close" href="#" onclick="this.parentNode.style.height=0"><i class="fas fa-times"></i></a>
+      <a target="_blank" href="https://vimeo.com/24754486"><i class="fab fa-vimeo"></i></a>
+      <a target="_blank" href="https://www.facebook.com/Delek-Childrens-Foundation-413781628796613/"><i class="fab fa-facebook-square"></i></a>
+      <a target="_blank" href="https://www.youtube.com/channel/UC7K3C7iSY1JO0HAy4P-Jn5w"><i class="fab fa-youtube"></i></a>
+      <a target="_blank" href="https://twitter.com/delekcf"><i class="fab fa-twitter"></i></a>
     </nav>
     <div
       style={{
