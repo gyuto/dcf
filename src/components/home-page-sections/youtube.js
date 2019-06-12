377ca97c46f76660044b267e11cb15fe2ef6ca02
Di Ye <imdiye@gmail.com>
2019-06-11 21:06:06 -0700

    test

--- src/components/home-page-sections/youtube.js
+++ src/components/home-page-sections/youtube.js
@@ -9,35 +9,59 @@ export default () => {
   const height = width === 1200 ? 600 : (clientWidth / 16) * 9
 
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        // textAlign: "center",
-        // paddingLeft: (clientWidth - width) / 2,
-      }}
-    >
+    <>
       <div
         style={{
-          border: "5px solid red",
-          width: width,
-          height: height,
-          position: "relative",
-          border: "1px solid red",
-          padding: 10,
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          // textAlign: "center",
+          // paddingLeft: (clientWidth - width) / 2,
         }}
       >
-        <iframe
-          width="100%"
-          height={height}
-          aligh="middle"
-          src="https://www.youtube.com/embed/VcFZmdjaAJ8"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
+        <div
+          style={{
+            border: "5px solid red",
+            width: width,
+            height: height,
+            position: "relative",
+            border: "1px solid red",
+            padding: 10,
+          }}
+        >
+          yo
+        </div>
       </div>
-    </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          // textAlign: "center",
+          // paddingLeft: (clientWidth - width) / 2,
+        }}
+      >
+        <div
+          style={{
+            border: "5px solid red",
+            width: width,
+            height: height,
+            position: "relative",
+            border: "1px solid red",
+            padding: 10,
+          }}
+        >
+          <iframe
+            width="100%"
+            height={height}
+            aligh="middle"
+            src="https://www.youtube.com/embed/VcFZmdjaAJ8"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    </>
   )
 }
