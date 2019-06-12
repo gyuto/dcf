41335684b528ebe72a1a1f4aed0aa81b81132a39
Di Ye <imdiye@gmail.com>
2019-06-11 21:47:52 -0700

    Revert "Hide social banner"
    This reverts commit e7b8d47510ef4e4453833c5a42c7c41b6282e361.

--- src/components/header.js
+++ src/components/header.js
@@ -11,14 +11,14 @@ const Header = ({ siteTitle }) => (
       boxShadow: "0 3px 5px #DDD",
     }}
   >
-    {/* <nav class="social">
+    <nav class="social">
       <div>Connect with <span>Delek Children&rsquo;s Foundation</span></div>
       <a target="_blank" href="https://vimeo.com/24754486"><i class="fab fa-vimeo"></i></a>
       <a target="_blank" href="https://www.facebook.com/Delek-Childrens-Foundation-413781628796613/"><i class="fab fa-facebook-square"></i></a>
       <a target="_blank" href="https://www.youtube.com/channel/UC7K3C7iSY1JO0HAy4P-Jn5w"><i class="fab fa-youtube"></i></a>
       <a target="_blank" href="https://twitter.com/delekcf"><i class="fab fa-twitter"></i></a>
       <a class="close" href="#" onclick="this.parentNode.style.height = '0px'"><i class="fas fa-times"></i></a>
-    </nav> */}
+    </nav>
     <div
       style={{
         textAlign: "center",
