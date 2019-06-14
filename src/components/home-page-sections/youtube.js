4872d194851aee5a2d3e9f9417f401494bf12878
Di Ye <imdiye@gmail.com>
2019-06-14 12:27:31 -0700

    Fix youtube video

--- src/components/home-page-sections/youtube.js
+++ src/components/home-page-sections/youtube.js
@@ -2,29 +2,12 @@ import React from "react"
 import CenteredBlock from "../centered-block"
 
 export default () => {
-  if (typeof window == "undefined") {
-    return null
-  }
-  const clientWidth = window.document.body.clientWidth
-  const width = clientWidth >= 1200 ? 1200 : "100%"
-  const height = width === 1200 ? 600 : (clientWidth / 16) * 9
-
   return (
     <CenteredBlock>
-      <div>
-        <p
-          style={{
-            border: "2px solid pink",
-          }}
-        >
-          yo
-        </p>
-      </div>
-
       <iframe
         width="100%"
-        height={height}
-        aligh="middle"
+        height={300}
+        align="middle"
         src="https://www.youtube.com/embed/VcFZmdjaAJ8"
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
