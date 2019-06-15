6b01933af3b118fd36d39225aaab6e5d877fab88
Di Ye <imdiye@gmail.com>
2019-06-14 17:51:54 -0700

    stopPropagation

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -6,7 +6,14 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
 export default ({ children, dismissModal }) => {
   return (
     <div className="modal-backdrop" onClick={dismissModal}>
-      <div className="modal-card">{children}</div>
+      <div
+        className="modal-card"
+        onClick={e => {
+          e.stopPropagation()
+        }}
+      >
+        {children}
+      </div>
       <div className="modal-dismiss-button" onClick={dismissModal}>
         <FontAwesomeIcon size={"1x"} icon={faArrowLeft} />
       </div>
