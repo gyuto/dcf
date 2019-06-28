5f3c5822928b065335d9a2d3ee84083df426935f
Di Ye <imdiye@gmail.com>
2019-06-28 15:56:51 -0700

    fix

--- src/components/student-profiles/index.js
+++ src/components/student-profiles/index.js
@@ -1,57 +1,77 @@
-import React, { useState } from "react"
+import React from "react"
 import content from "./content"
 import Modal from "../modal"
 import Profile from "./profile"
 import "./styles.scss"
+import initFirestore from "../../lib/firebase"
 
-export default ({ count = 0 }) => {
-  const [modal, updateModalState] = useState({
-    on: false,
+const bs = async () => {
+  // if not init yet. init it.
+  await initFirestore()
+}
+class StudentProfiles extends React.Component {
+  state = {
+    modalOn: false,
     studentIndex: 0,
-  })
+  }
 
-  return (
-    <>
-      {modal.on && (
-        <Modal
-          dismissModal={() => {
-            updateModalState({
-              on: false,
-            })
+  render() {
+    const { modalOn, studentIndex } = this.state
+    const { count = 0 } = this.props
+
+    return (
+      <div>
+        {modalOn && (
+          <Modal
+            dismissModal={() => {
+              this.updateModalState({
+                modalOn: false,
+              })
+            }}
+          >
+            <Profile profile={content[studentIndex]} />
+          </Modal>
+        )}
+        <div
+          className="student-profile-container"
+          style={{
+            justifyContent: count ? "center" : null,
           }}
         >
-          <Profile profile={content[modal.studentIndex]} />
-        </Modal>
-      )}
-      <div
-        className="student-profile-container"
-        style={{
-          justifyContent: count ? "center" : null,
-        }}
-      >
-        {/* Only render the first ${count} student profiles */}
-        {(count ? content.slice(0, count) : content).map((student, index) => {
-          return (
-            <div
-              onClick={() => {
-                updateModalState({
-                  on: true,
-                  studentIndex: index,
-                })
-              }}
-              key={student.name + index}
-              className="student-profile-pic"
-            >
-              <img
-                src={student.pic}
-                style={{
-                  width: "100%",
+          {/* Only render the first ${count} student profiles */}
+          {(count ? content.slice(0, count) : content).map((student, index) => {
+            return (
+              <div
+                onClick={() => {
+                  this.updateModalState({
+                    modalOn: true,
+                    studentIndex: index,
+                  })
                 }}
-              />
-            </div>
-          )
-        })}
+                key={student.name + index}
+                className="student-profile-pic"
+              >
+                <img
+                  src={student.pic}
+                  style={{
+                    width: "100%",
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
       </div>
-    </>
-  )
+    )
+  }
+
+  componentDidMount() {
+    bs()
+  }
+
+  updateModalState = state => {
+    this.setState(state)
+  }
 }
+
+export default StudentProfiles
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase"
-const keys = process.env
+
 const config = {
-  apiKey: keys.FIREBASE_API_KEY,
-  authDomain: keys.FIREBASE_AUTH_DOMAIN,
-  databaseURL: keys.FIREBASE_DATABASE_URL,
-  projectId: keys.FIREBASE_PROJECT_ID,
-  storageBucket: keys.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: keys.FIREBASE_MESSAGING_SENDER_ID,
-  appId: keys.FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 const init = async () => {
