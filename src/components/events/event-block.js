72ad6924819052d2f8ac5acb2f4e9335b5373f02
Di Ye <imdiye@gmail.com>
2019-07-26 15:46:22 -0700

    event block

--- src/components/events/event-block.js
+++ src/components/events/event-block.js
@@ -1,51 +1,68 @@
 import React from "react"
-import moment from "moment-timezone"
+// import moment from "moment-timezone"
+import moment from "moment"
 import Card from "../card"
+import Spacer from "../spacer"
 import CenteredBlock from "../centered-block"
 import * as colors from "../../styles/colors"
 
 const Event = ({ data }) => {
-  const startTime = moment(data.startTime.seconds * 1000).tz(
-    "America/Los_Angeles"
-  )
+  const startTime = moment(data.startTime.seconds * 1000)
+  const endTime = moment(data.endTime.seconds * 1000)
 
   return (
     <CenteredBlock>
       <Card padding={20}>
-        {/* <div
-          style={{
-            width: 350,
-            height: 430,
-            backgroundImage: `url(${data.pic})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-        /> */}
         <div
           style={{
             display: "flex",
+            justifyContent: "space-between",
           }}
         >
-          <h3
+          <div
             style={{
-              width: 150,
-              textAlign: "center",
+              display: "flex",
             }}
           >
-            <span
+            <h3
               style={{
-                fontSize: 45,
-                color: colors.brandSecondary,
+                width: 150,
+                textAlign: "center",
               }}
             >
-              {startTime.format("DD")}
-            </span>
-            <br />
-            {startTime.format("MMM")}
-          </h3>
-          <div>
-            <>{data.name}</>
+              <span
+                style={{
+                  fontSize: 45,
+                  color: colors.brandSecondary,
+                }}
+              >
+                {startTime.format("DD")}
+              </span>
+              <br />
+              {startTime.format("MMM")}
+            </h3>
+            <div>
+              <Spacer height={10} />
+              <h3>{data.name}</h3>
+              <p>
+                {startTime.format("ddd")}, {startTime.format("LT")} -{" "}
+                {endTime.format("LT")}
+              </p>
+              <p>{data.address}</p>
+            </div>
           </div>
+
+          <a
+            style={{
+              backgroundImage: `url(${data.pic})`,
+              width: 300,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              opacity: 0.8,
+            }}
+            href={data.pic}
+            target="_blank"
+          />
         </div>
       </Card>
     </CenteredBlock>
